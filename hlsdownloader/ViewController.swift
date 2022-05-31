f40e860dd9db6f3239cc11b2941cdd1183418b37
LeeChooHyoung <leechoohyoung@gmail.com>
2022-05-31 10:31:28 +0900

    feat: add project files

---
+++ hlsdownloader/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  hlsdownloader
+//
+//  Created by ChooHyoungLee on 2022/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "subtitleCell")
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "subtitleCell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        cell.detailTextLabel?.text = "sub\(indexPath.row)"
+        return cell
+    }
+}
